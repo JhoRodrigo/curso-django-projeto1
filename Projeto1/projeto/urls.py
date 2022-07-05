b4c5eadb49a043148bfc08a9c2425af7e53cb09d
Jho Rodrigo <jrodrigomdc@hotmail.com>
2022-07-05 00:05:19 -0300

    ✅ Aula 23

--- Projeto1/projeto/urls.py
+++ Projeto1/projeto/urls.py
@@ -14,15 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.http import HttpResponse
-from django.urls import path
-
-
-def my_view(request):
-    return HttpResponse('Uma linda String')
-
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sobre/', my_view)
+    # Include e uma função do Django para inclusão de urls dos apps
+    path('recipes', include('recipes.urls'))
+
 ]
